23fb09cad9335102494c140a419dd18ef613c1ef
RichardP <rdpgamedev@gmail.com>
2016-10-28 18:46:42 -0700

    Reformatting
    Reformatting for consistent, clean style.
    Based off of Google C++ style guide.

--- Pascal'sTriangle.cpp
+++ Pascal'sTriangle.cpp
@@ -1,7 +1,7 @@
 // 
 // Pascal'sTriangle.cpp : Implementation of function pascal_val.
 // Finds values of Pascal's Triangle given by row and position.
-// Richard Pittman - 10/27/16
+// Richard Pittman - 10/28/16
 //
 
 #include <assert.h>
@@ -18,21 +18,16 @@ typedef std::map<coord, unsigned __int64> trimap;
 //uses trimap triangle to record previously computed values
 unsigned __int64 pascal_val(int row, int pos, trimap* triangle)
 {
-	//check input
-	if (row < 0 || pos < 0 || pos > row) 
-		throw 1;
-	//check if edge of row
-	if (pos == 0 || pos == row)
-	{
-		return 1;
-	}
+	if (row < 0 || pos < 0 || pos > row) throw 1; //check input for illegal coord
+	if (pos == 0 || pos == row) return 1; //check if edge of row
 	//recursive call
 	//find answer in triangle
 	coord coords(pos, row);
 	trimap::iterator value = triangle->find(coords);
 	if (value == triangle->end()) //value not recorded
 	{
-		unsigned __int64 answer = pascal_val(row - 1, pos - 1, triangle) + pascal_val(row - 1, pos, triangle);
+    unsigned __int64 answer = pascal_val(row - 1, pos - 1, triangle) + 
+        pascal_val(row - 1, pos, triangle);
 		triangle->insert(std::pair<coord, unsigned __int64>(coords, answer));
 		return answer;
 	}
@@ -63,12 +58,14 @@ int main()
 		switch (e) 
 		{
 		case 1: //illegal input exception
-			std::cerr << "Illegal Input in pascal_val(row, pos)" << '\n'
-				<< "Usage: ensure row and pos are legal pascal position values" 
-				<< '\n';
-			break;
+    {
+      std::cerr << "Illegal Input in pascal_val(row, pos)" << '\n'
+          << "Usage: ensure row and pos are legal pascal position values"
+          << '\n';
+      break;
+    }
 		default:
-			break;
+      break;
 		}
 	}
 	return 0;
